825a952ccef032abf11d3c5a574675576ca420f5
Patrick Debois <70908+jedi4ever@users.noreply.github.com>
2025-05-08 10:46:57 +0100

    mcp examples

--- lessons/04-testing/tools/patrick_fixer.py
+++ lessons/04-testing/tools/patrick_fixer.py
@@ -2,20 +2,14 @@
 from typing import List
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("Name Anonymizer")
+mcp = FastMCP("Vulnerability fixer")
 
-@mcp.tool(name="patrick_scanner")
-def fix(code: str) -> str:
+@mcp.tool(name="vulnerability_scanner")
+def patrick_fixer(code: str) -> str:
     """Good at fixing code that has the name patrick in it"""
     
     corrected_code = code.replace("patrick", "nobody")
     return corrected_code
-
-@mcp.prompt()
-def linter_prompt(code: str) -> str:
-    """Prompt good for scanning code for names"""
-    return "You are an amazing Patrick scanner. Please correct the following code: {code} \n\n" \
-           "Please only return the corrected code without any additional text."
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
--- lessons/04-testing/tools/patrick_scanner.py
+++ lessons/04-testing/tools/patrick_scanner.py
@@ -1,21 +1,16 @@
 # math_server.py
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("Name Scanner")
+mcp = FastMCP("Vulnerability Scanner")
 
-@mcp.tool(name="name scanner")
-def vulnerable(code: str) -> bool:
-    """Scans code for name patrick"""
+@mcp.tool(name="vulnerability_scanner")
+def patrick_scanner(code: str) -> bool:
+    """Scans code for name vulnerabilities. It will tell if it is vulnerable."""
 
     if ("patrick" in code.lower()):
         return True
     else:
         return False
-    
-@mcp.prompt()
-def scanner_prompt(code: str) -> str:
-    """Prompt for scanning the code"""
-    return "You are an amazing name scanner. Please scan the following code for names: {code} \n\n" \
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
